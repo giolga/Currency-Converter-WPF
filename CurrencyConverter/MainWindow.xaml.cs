16195ce65d4adaa6244545d9cf5634056cd8acc4
giolga <giolga.kumi@gmail.com>
2024-05-06 21:06:54 +0400

    CRUD operations BUG in UD

--- CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -30,13 +31,10 @@ namespace CurrencyConverter
             InitializeComponent();
             lblCurrency.Content = "Hello Kumi";
             BindCurrency();
+
+            GetData();
         }
 
-        //!!!
-        public void MyConnection()
-        {
-            String Connection = ConfigurationManager.ConnectionStrings["CurrencyDbContext"].ConnectionString;
-        }
 
         private void BindCurrency()
         {
@@ -108,6 +106,7 @@ namespace CurrencyConverter
 
         private void ClearControls()
         {
+
             txtCurrency.Text = string.Empty;
             if (cmbFromCurrency.Items.Count > 0)
             {
@@ -121,6 +120,17 @@ namespace CurrencyConverter
             lblCurrency.Content = "";
             txtCurrency.Focus();
         }
+
+        private void Clear_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Hand;
+        }
+
+        private void Clear_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
@@ -132,51 +142,191 @@ namespace CurrencyConverter
 
         }
 
+        //!!!
+        private CurrencyModel currencyModel;
+
         private void dgvCurrency_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
+            try
+            {
+                DataGrid dataGrid = (DataGrid)sender; //Create Object for DataGrid
+                DataRowView row_selected = dataGrid.CurrentItem as DataRowView; // For DataRowView
 
+
+                using (CurrencyDbContext context = new CurrencyDbContext())
+                {
+
+                    CurrencyModel selectedModel = dataGrid.CurrentItem as CurrencyModel;
+
+                    var myDb = context.CurrencyModels.ToList();
+
+                    if (selectedModel != null)
+                    {
+                        if (dgvCurrency.Items.Count > 0)
+                        {
+                            var model = myDb.Where(m => m.Id == selectedModel.Id).FirstOrDefault();
+
+                            MessageBox.Show($"Id {model.Id}");
+
+                            if (dataGrid.SelectedCells[0].Column.DisplayIndex == 0)
+                            {
+                                txtAmount.Text = model.Amount.ToString();
+                                txtCurrencyName.Text = model.CurrencyName.ToString();
+                                btnSave.Content = "Update";
+
+
+                                context.Entry(model).State = EntityState.Modified;
+                                context.SaveChanges();
+                                GetData();
+
+                            }
+
+                            if (dataGrid.SelectedCells[0].Column.DisplayIndex == 1)
+                            {
+                                context.CurrencyModels.Remove(model);
+                                context.SaveChanges();
+                                GetData();
+                            }
+                        }
+                    }
+                }
+
+                #region commented
+
+                //using (CurrencyDbContext context = new CurrencyDbContext())
+                //{
+                //    var model = context.CurrencyModels.ToList();
+
+                //    if (row_selected != null)
+                //    {
+                //        if (dgvCurrency.Items.Count > 0)
+                //        {
+                //            var selectedModel = model.Where(m =>
+                //                m.CurrencyName == row_selected["CurrencyName"].ToString() &&
+                //                m.Amount == int.Parse(row_selected["Amount"].ToString())).FirstOrDefault();
+
+                //            MessageBox.Show($"Id {selectedModel.Id}");
+
+                //            if (dataGrid.SelectedCells[0].Column.DisplayIndex == 0)
+                //            {
+                //                txtAmount.Text = row_selected["Amount"].ToString();
+                //                txtCurrencyName.Text = row_selected["CurrencyName"].ToString();
+                //                btnSave.Content = "Update";
+                //            }
+
+                //            if (dataGrid.SelectedCells[0].Column.DisplayIndex == 1)
+                //            {
+
+                //            }
+                //        }
+                //    }
+
+                //}
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (txtAmount.Text == null || txtAmount.Text == "")
+                if (btnSave.Content != "Update")
                 {
-                    MessageBox.Show("Please enter amount", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
-                    txtAmount.Focus();
-                    return;
-                }
-                else if (txtCurrencyName.Text == null || txtCurrencyName.Text == "")
-                {
-                    MessageBox.Show("Please enter currency amount", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
-                    txtCurrencyName.Focus();
-                    return;
-                }
-                else
-                {
-                    //insert into Database
-                    using(CurrencyDbContext currencyDbContext = new CurrencyDbContext())
+
+                    if (txtAmount.Text == null || txtAmount.Text == "")
                     {
-                        CurrencyModel currencyModel = new CurrencyModel();
-
-                        currencyModel.Amount = int.Parse(txtAmount.Text);
-                        currencyModel.CurrencyName = txtCurrencyName.Text;
-
-                        currencyDbContext.CurrencyModels.Add(currencyModel);
-                        currencyDbContext.SaveChanges();
+                        MessageBox.Show("Please enter amount", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        txtAmount.Focus();
+                        return;
                     }
+                    else if (txtCurrencyName.Text == null || txtCurrencyName.Text == "")
+                    {
+                        MessageBox.Show("Please enter currency amount", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        txtCurrencyName.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        if (MessageBox.Show("Are you sure you want to delete all info from DB?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                        {
+                            //insert into Database
+                            using (CurrencyDbContext currencyDbContext = new CurrencyDbContext())
+                            {
+                                CurrencyModel currencyModel = new CurrencyModel();
+
+                                currencyModel.Amount = int.Parse(txtAmount.Text);
+                                currencyModel.CurrencyName = txtCurrencyName.Text;
+
+                                currencyDbContext.CurrencyModels.Add(currencyModel);
+                                currencyDbContext.SaveChanges();
+
+                                GetData();
+                            }
+                        }
+                    }
+
                 }
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            finally {
+                btnSave.Content = "Save";
+            }
+
+
         }
+
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
+            if (btnSave.Content == "Save")
+            {
+                btnSave.Content = "Save";
+            }
 
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to delete all info from DB?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+
+                    using (CurrencyDbContext context = new CurrencyDbContext())
+                    {
+                        context.CurrencyModels.RemoveRange(context.CurrencyModels);
+                        context.SaveChanges();
+                    }
+
+                    GetData();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+
+        public void GetData()
+        {
+            using (CurrencyDbContext context = new CurrencyDbContext())
+            {
+                if (context.CurrencyModels != null)
+                {
+                    var model = context.CurrencyModels.ToList();
+                    //MessageBox.Show($"Here is {model.Count} rows");
+                    dgvCurrency.ItemsSource = model;
+                }
+            }
+        }
+
+
     }
 }
